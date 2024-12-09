e860ce0071d690e839653ac4a9624bd6eb886d71
kimseongwoo <mdflagZ@gmail.com>
2024-12-09 14:56:51 +0900

    Update main.c

--- FinalExam/TestGame/main.c
+++ FinalExam/TestGame/main.c
@@ -1,7 +1,8 @@
-﻿#include "ElfMath.h"
+#include "ElfMath.h"
 #include "Elf2D.h"
 #include "ElfTime.h"
 #include "ElfObject.h"
+#include "conio.h"
 
 #define WIDTH 40    // 가로 픽셀 개수
 #define HEIGHT 24   // 세로 픽셀 개수
@@ -64,8 +65,8 @@ int Input()
 }
 
 void Update(GameObject_Line* obj, int objNum, int e)
-{    
-    
+{
+
 }
 
 void Render(GameObject_Line* obj, int objNum, char* Buf, int width, int height)
@@ -77,34 +78,34 @@ void Render(GameObject_Line* obj, int objNum, char* Buf, int width, int height)
     lineB.y = 0;
 
     int i = 0;
-   
-        obj[i].Position.x = lineB.x;
-        obj[i].Position.y = lineB.y;
 
-        lineA.x = obj[i].Line[0].x;
-        lineA.y = obj[i].Line[0].y;
-        lineA.z = 1;
-        lineB.x = obj[i].Line[1].x;
-        lineB.y = obj[i].Line[1].y;
-        lineB.z = 1;
+    obj[i].Position.x = lineB.x;
+    obj[i].Position.y = lineB.y;
 
-        Matrix3x3 scale = scale_matrix(obj[i].Scale.x, obj[i].Scale.y); // 크기조정 행렬 적용
-        Matrix3x3 rotation = rotation_matrix(obj[i].Rotation);// 회전 행렬 생성
-        Matrix3x3 translation = translation_matrix(obj[i].Position.x, obj[i].Position.y); // 이동 행렬 적용
-        
-        world = multiply_matrices(scale, world);
-        world = multiply_matrices(rotation, world);
-        world = multiply_matrices(translation, world);
-        lineA = multiply_matrix_vector(world, lineA);  // 점과 회전 행렬 곱셈 (Matrix3x3 * Vector3)
-        lineB = multiply_matrix_vector(world, lineB);  // 점과 회전 행렬 곱셈 (Matrix3x3 * Vector3)
+    lineA.x = obj[i].Line[0].x;
+    lineA.y = obj[i].Line[0].y;
+    lineA.z = 1;
+    lineB.x = obj[i].Line[1].x;
+    lineB.y = obj[i].Line[1].y;
+    lineB.z = 1;
 
-        Elf2DDrawLine((int)lineA.x, (int)lineA.y, (int)lineB.x, (int)lineB.y, Buf, width, height);
+    Matrix3x3 scale = scale_matrix(obj[i].Scale.x, obj[i].Scale.y); // 크기조정 행렬 적용
+    Matrix3x3 rotation = rotation_matrix(obj[i].Rotation);// 회전 행렬 생성
+    Matrix3x3 translation = translation_matrix(obj[i].Position.x, obj[i].Position.y); // 이동 행렬 적용
+
+    world = multiply_matrices(scale, world);
+    world = multiply_matrices(rotation, world);
+    world = multiply_matrices(translation, world);
+    lineA = multiply_matrix_vector(world, lineA);  // 점과 회전 행렬 곱셈 (Matrix3x3 * Vector3)
+    lineB = multiply_matrix_vector(world, lineB);  // 점과 회전 행렬 곱셈 (Matrix3x3 * Vector3)
+
+    Elf2DDrawLine((int)lineA.x, (int)lineA.y, (int)lineB.x, (int)lineB.y, Buf, width, height);
 }
 
 void Elf2DDrawline2(Point start, Point end, char draw_char) {
     float dx = end.x - start.x;
     float dy = end.y - start.y;
-    int steps = (fabs(dx) > fabs(dy)) ? fabs(dx) : fabs(dy);
+    int steps = (fabs(dx) > fabs(dy)) ? (int)fabs(dx) : (int)fabs(dy);
 
     float x_inc = dx / steps;
     float y_inc = dy / steps;
@@ -125,6 +126,19 @@ void Elf2DDrawline2(Point start, Point end, char draw_char) {
     }
 }
 
+// 삼각형 그리기 함수
+void DrawTriangle(Point center, float distance, char draw_char) {
+    // 삼각형 꼭짓점 계산
+    Point top = { center.x, center.y - distance };                   // 위쪽 꼭짓점
+    Point left = { center.x - distance / 2, center.y + distance / 2 }; // 왼쪽 꼭짓점
+    Point right = { center.x + distance / 2, center.y + distance / 2 }; // 오른쪽 꼭짓점
+
+    // 삼각형의 변을 그리기
+    Elf2DDrawline2(top, left, draw_char);   // 왼쪽 변
+    Elf2DDrawline2(left, right, draw_char); // 아래 변
+    Elf2DDrawline2(right, top, draw_char);  // 오른쪽 변
+}
+
 // 게임 루프
 int main() {
     int fps = 30;
@@ -139,18 +153,19 @@ int main() {
 
     // 게임 초기화
     Initialize(LineObj, 3);
-    
+
     Elf2DInitScreen();
     Elf2DClearScreen(screenBuffer, screenWidth, screenHeight);
     Elf2DDrawBuffer(screenBuffer);
-    
+
     ElfTimeInitialize();
-    
+
+
     int isGameRunning = 1;
     while (isGameRunning) {
-        
+
         int gameEvent = Input();
-        
+
         //입력 계산
         if (gameEvent == 99)
         {
@@ -159,15 +174,15 @@ int main() {
 
         // 게임 업데이트
         Update(LineObj, 3, gameEvent);
-        
+
         // 스크린버퍼에 게임 렌더링
         Render(LineObj, 3, screenBuffer, screenWidth, screenHeight);
 
         // 화면 그리기
         Elf2DDrawBuffer(screenBuffer);
-        
+
         ElfTimeCalculateDeltaTime();
-        Elf2DSleep( frameTime - ElfTimeGetDeltaTime() );
+        Elf2DSleep((int)(frameTime - ElfTimeGetDeltaTime()));
     }
 
     return 0;
